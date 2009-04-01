819957b83bf8a2bd62e3d032de399a61d98d12b3
Ryan Daigle <ryan.daigle@gmail.com>
2009-04-01 11:21:33 -0400

    Updated deployment for EY Solo

---
+++ config/deploy.rb
@@ -0,0 +1,51 @@
+# Please install the Engine Yard Capistrano gem
+# gem install eycap --source http://gems.engineyard.com
+require "eycap/recipes"
+
+set :keep_releases, 5
+set :application,   'RemoteLogger'
+set :repository,    'git@github.com:yfactorial/remotelogger-server.git'
+set :deploy_to,     "/data/#{application}"
+set :deploy_via,    :export
+set :monit_group,   "#{application}"
+set :scm,           :git
+
+# This is the same database name for all environments
+set :production_database,'RemoteLogger_production'
+
+set :environment_host, 'localhost'
+set :deploy_via, :remote_cache
+
+# uncomment the following to have a database backup done before every migration
+before "deploy:migrate", "db:dump"
+
+# comment out if it gives you trouble. newest net/ssh needs this set.
+ssh_options[:paranoid] = false
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true # required for svn+ssh:// andf git:// sometimes
+
+# This will execute the Git revision parsing on the *remote* server rather than locally
+set :real_revision, 			lambda { source.query_revision(revision) { |cmd| capture(cmd) } }
+
+task :labs do
+  role :web, '174.129.222.32'
+  role :app, '174.129.222.32'
+  role :db, '174.129.222.32', :primary => true
+  set :environment_database, Proc.new { production_database }
+  set :dbuser,        'deploy'
+  set :dbpass,        'meeyIf05FK'
+  set :user,          'deploy'
+  set :password,      'meeyIf05FK'
+  set :runner,        'deploy'
+end
+
+
+# TASKS
+# Don't change unless you know what you are doing!
+after "deploy", "deploy:cleanup"
+after "deploy:migrations", "deploy:cleanup"
+after "deploy:update_code","deploy:symlink_configs"
+after "deploy:update", "newrelic:notice_deployment"
+
+
