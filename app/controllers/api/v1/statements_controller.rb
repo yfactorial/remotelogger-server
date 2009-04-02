ed170ab4549b6bc890780157725eaf52d2f5affd
Ryan Daigle <ryan.daigle@gmail.com>
2009-04-02 11:09:06 -0400

    Added explicit handling of format.json

--- app/controllers/api/v1/statements_controller.rb
+++ app/controllers/api/v1/statements_controller.rb
@@ -11,18 +11,21 @@ module Api
           format.html { render :text => current_object }
           format.xml { render :xml => current_object }
           format.js { render :json => current_object }
+          format.json { render :json => current_object }
         end
 
         response_for(:create) do |format|
           format.html { render :text => 'Statement successfully created', :status => :created }
           format.xml { render :xml => current_object, :status => :created, :location => api_v1_statement_url(current_object, :format => :xml) }
           format.js { render :json => current_object, :status => :created, :location => api_v1_statement_url(current_object, :format => :json) }
+          format.json { render :json => current_object, :status => :created, :location => api_v1_statement_url(current_object, :format => :json) }
         end
         
         response_for(:create_fails) do |format|
           format.html { render :text => current_object.errors.full_messages.join('\n'), :status => 400 }
           format.xml { render :xml => current_object.errors, :status => 400 }
           format.js { render :json => current_object.errors, :status => 400 }
+          format.json { render :json => current_object.errors, :status => 400 }
         end
       end
       
